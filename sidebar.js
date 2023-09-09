81d8a7569acbdf3e73377c3be395d8cd56ad3de7
shourya102 <shourya.sahu456@gmail.com>
2023-09-09 15:20:40 +0530

    finished sidebar

--- sidebar.js
+++ sidebar.js
@@ -3,4 +3,5 @@ const closebut = document.getElementById("close");
 closebut.addEventListener("click", () =>
 {   
     document.querySelector('.sidebar').classList.toggle('closed');
+    document.querySelector('.right').classList.toggle('closed1');
 });
