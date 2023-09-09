9b865c79af629631beeffee36b9aa0748f2ea2b1
shourya102 <shourya.sahu456@gmail.com>
2023-09-09 17:30:28 +0530

    fixed files

--- server.js
+++ server.js
@@ -4,4 +4,16 @@ import ejs from 'ejs';
 const app= express();
 
 
+app.set('views-engine', ejs);
+app.use('/public', express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('index.ejs');
+});
+
+app.listen(3000, ()=>{
+    console.log("Server is running at port 3000");
+})
+
+
 
