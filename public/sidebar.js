d12225c1b03c063075c816e3f1fd3578c547a620
shourya102 <shourya.sahu456@gmail.com>
2023-09-11 18:39:20 +0530

    initiated react migration

--- public/sidebar.js
+++
@@ -1,7 +0,0 @@
-const closebut = document.getElementById("close");
-
-closebut.addEventListener("click", () =>
-{   
-    document.querySelector('.sidebar').classList.toggle('closed');
-    document.querySelector('.right').classList.toggle('closed1');
-});
