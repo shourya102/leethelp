1f7a11820a1fadeae8497df03e553a4db63342f3
shourya102 <shourya.sahu456@gmail.com>
2023-09-14 00:04:31 +0530

    made sign in page

---
+++ src/components/home/Home.js
@@ -0,0 +1,16 @@
+import './Home.css';
+
+export default function Home() {
+  return (
+      <div className="form-container">
+          <form>
+              <h1>Welcome to Leethelp>> </h1>
+              <label htmlFor="username">Username</label>
+              <input type="text" id='username'/>
+              <label htmlFor="password">Password</label>
+              <input type="text" id='password'/>
+              <button>Log In</button>
+          </form>
+      </div>
+  );
+};
